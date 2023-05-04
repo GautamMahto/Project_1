84f6e32f0421d48e307ad5e9130ee2428a87b57b
GautamMahto <gautammahtoaiie31aii.ac.in@gmail.com>
2023-05-04 20:10:18 +0530

    Data Validation

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from Insurance.exception import InsuranceException
 import os,sys
 from Insurance.utils import get_collection_as_dataframe
 from Insurance.entity.config_entity import DataIngestionConfig
+from Insurance.entity.config_entity import DataValidationConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
+from Insurance.components.data_validation import DataValidation
 
 # def test_logger_and_exception():
 #     try:
@@ -26,6 +28,11 @@ if __name__== "__main__":
 
         data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        
+        # Data Validatition
+        data_validation_config=config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation=DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
+        data_validation_artifact=data_validation.inititate_data_validation()
 
     except Exception as e:
         print(e)
